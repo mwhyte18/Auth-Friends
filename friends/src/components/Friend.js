d43f909ca92ea5f4f9126fc5618c1ffcd7027c9a
Marco Whyte <mwhyte@umass.edu>
2020-02-25 21:48:21 -0600

    finished mvp

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+function Friend(props) {
+  const [friend, setFriend] = useState({});
+
+  useEffect(() => {
+    setFriend(props.friend);
+  }, [props.friend]);
+
+  function deleteFriend() {
+    axiosWithAuth()
+      .delete(`/api/friends/${friend.id}`)
+      .then(res => {
+        alert("Congrats you lost another friend");
+        console.log("deleted", res);
+      })
+      .catch(err => console.log("error deleting friend", err))
+      .finally(() => window.location.reload());
+  }
+
+  return (
+    <div className="friend">
+      <h1>{friend.name}</h1>
+      <p>Email: {friend.email}</p>
+      <p>Age: {props.friend.age}</p>
+      <button onClick={deleteFriend}>block this friend</button>
+    </div>
+  );
+}
+
+export default Friend;
